2ab2cd2078531665b487d80802cade6e7736bceb
Dickson Eneh <dickseneh99@gmail>
2022-06-11 07:07:25 +0200

    api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
-const port = 1999
+const port = process.env.PORT || 1999
 const mongoose =require("mongoose")
                 //  "mongodb+srv://socialapp:today@cluster0.xd5wm.mongodb.net/socialapp?"
 mongoose.connect("mongodb+srv://foodapp:today@cluster0.xd5wm.mongodb.net/foodapp?").then(()=>{
